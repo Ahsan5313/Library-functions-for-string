b1211690406482b1ddc48494e3003a7f05f83f1e
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-04 11:15:17 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,12 @@
+var f_name= prompt("enter your first name : ");
+var l_name= prompt("enter your last name : ");
+document.write("the number of all charcter " + f_name+l_name.length);
+
+var text="ahsaunl";
+var text1=" huq";
+document.write(text+text1);
+var text2="ahsanul";
+var text3=" huq";
+var textAdd=(text2+text3);
+var textPrint=(textAdd.toUpperCase());
+document.write(textPrint.charAt(2));
